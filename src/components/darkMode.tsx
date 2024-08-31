12a25eb044c589324e99ea13e2b23869b358fa9d
Subha060 <shubhaadak0@gmail.com>
2024-08-31 09:56:47 +0530

    first commit

---
+++ src/components/darkMode.tsx
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from 'react';
+
+
+
+
+
+
+
+function modeChange(isChecked) {
+    if (isChecked) {
+        document.body.classList.add('dark-mode');
+    } else {
+        document.body.classList.remove('dark-mode');
+    }
+}
+
+export default function DarkMode() {
+    const [isChecked, setIsChecked] = useState(false);
+
+    useEffect(() => {
+        modeChange(isChecked);
+    }, [isChecked]);
+
+    const handleCheckboxChange = () => {
+        setIsChecked(!isChecked);
+    };
+
+    return (
+        <div className='absolute right-8 top-16'>
+            <div className="wrap-check-61">
+                <input 
+                    type="checkbox" 
+                    className='checkbox' 
+                    checked={isChecked} 
+                    onChange={handleCheckboxChange} 
+                />
+            </div>
+        </div>
+    );
+}
---
+++ src/components/mouseFollow.tsx
@@ -0,0 +1,53 @@
+import React, { useState, useEffect } from 'react';
+import '../App.css'; // Import your CSS file
+
+const MouseCursor = () => {
+  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+  const [isVisible, setIsVisible] = useState(window.innerWidth > 700);
+
+  useEffect(() => {
+    const updatePosition = (e) => {
+      setMousePosition({
+        x: e.clientX,
+        y: e.clientY
+      });
+    };
+
+    const handleResize = () => {
+      setIsVisible(window.innerWidth > 1000);
+    };
+
+    document.addEventListener('mousemove', updatePosition);
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      document.removeEventListener('mousemove', updatePosition);
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  const { x, y } = mousePosition;
+
+  return (
+    <>
+      {isVisible && (
+        <>
+          <div
+            className="mouseBody"
+            style={{
+              transform: `translate(${x - 35}px, ${y - 35}px)` // Assuming 50px width/height
+            }}
+          />
+          <div
+            className="mouseDot"
+            style={{
+              transform: `translate(${x - 10}px, ${y - 10}px)` // Assuming 8px width/height
+            }}
+          />
+        </>
+      )}
+    </>
+  );
+};
+
+export default MouseCursor;
